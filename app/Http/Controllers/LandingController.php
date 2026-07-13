542ac121499544689d232bb63ab79b48a69500e4
MohamedKhaled31 <mohamedkhaled3119@gmail.com>
2026-07-13 17:11:08 +0300

    update home redirect

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -6,6 +6,13 @@ namespace App\Http\Controllers;
 class LandingController extends Controller
 {
     public function home(){
+        if (auth()->check()) {
+            if (auth()->user()->role === 'admin') {
+                return redirect()->route('admin.dashboard');
+            } else {
+                return redirect()->route('wallet.index');
+            }
+        }
         return view('landing.home');
     }
 }
