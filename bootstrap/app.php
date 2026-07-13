8ed9ef3e4f1983a2c18af355def73dfff540573e
MohamedKhaled31 <mohamedkhaled3119@gmail.com>
2026-07-13 17:33:15 +0300

    fix https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -18,9 +18,13 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => IsAdmin::class,
             'user' => IsUser::class,
         ]);
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
+            fn(Request $request) => $request->is('api/*'),
         );
     })->create();
